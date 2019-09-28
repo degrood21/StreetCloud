accb0a07ab27a2eb3efaa7d2c6c7ad2fa4a2b877
Paul P T Bellosillo <lozinalaifer.3791@gmail.cm>
2019-09-27 18:38:57 -0700

    HTML reformatting
    added the scrollable menu and filters to the pages. Not all had been formatted

--- StreetCloud/routes/index.js
+++ StreetCloud/routes/index.js
@@ -3,6 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /**
+   * External Citation
+   * Problem: Didn't know how to set home page as default
+   * Date: September 25, 2019
+   * Resource: https://stackoverflow.com/questions/21119288/simplest-way-to-have-express-serve-a-default-page
+   * Resolution: Used sendFile solution found on this forum
+   */
   res.sendFile('streetcloud.html', { root: __dirname + "/../public"});
 });
 
