d6880e5223e81f968eeb6e9625a938dcc582e9a1
David Campbell <campbeld21@up.edu>
2019-10-27 13:23:10 -0700

    Update searchPage.js

--- StreetCloud/routes/searchPage.js
+++ StreetCloud/routes/searchPage.js
@@ -7,9 +7,7 @@ router.post('/',function(req,res){
     var query = body.inquiry;
     
     var searchCatergories = ["food", "shelter", "medical"];
-    var searchResultsArray = {
-        "": 
-    };
+    var searchResultsArray = [""];
 
     for (var index = 0; index < searchCatergories.length; index++){
     
