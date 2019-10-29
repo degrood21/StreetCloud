0f8900739ab3ccce76d7b72aceec523bd17755e4
Paul P T Bellosillo <lozinalaifer.3791@gmail.cm>
2019-10-29 14:13:21 -0700

    Allowed search to call select statements on multiple tables

--- StreetCloud/public/javascripts/streetcloudNavigation.js
+++ StreetCloud/public/javascripts/streetcloudNavigation.js
@@ -32,6 +32,31 @@ document.getElementById("homeButton").onclick = function() {
 };
 });
 
+document.getElementById("searchButton").onclick = function() {
+    var searchFor = $("#searchText").val();
+    console.log("Searching for: " + searchFor);
+    var async = require('async');
+    location.href = "/../streetcloud_gen_search.html";
+
+    async.waterfall([
+        $(document).ready(function(){
+            $.post('/searchPage',
+            {
+                inquiry: searchFor
+            },
+            function(data){
+                $(document).ready(function(){
+                for (i = 1; i < data.length; i++){
+                    $("#genResults").append("<tr><td><p>Name: "+data[i].NAME+"</p></td>");
+                    $("#genResults").append("<tr><td><p>Address: "+data[i].ADDRESS+"</p></td>");
+                    $("#genResults").append("<tr><td><p>Distance: "+data[i].DISTANCE+"</p></td>");
+                }
+                });
+            });
+        });
+    ]
+}
+
 //these fucntions prints the data from the database 
 function medicalFunction(){
     $(document).ready(function(){
@@ -73,18 +98,39 @@ function shelterFunction(){
     });
 }
 
-// Searches for results from database with the same name
+/* Searches for results from database with the same name
 function search(){
-    var searchFor = document.getElementById("searchText").innerHTML;
-    console.log("Searching for" + searchFor);
     location.href = "/../streetcloud_gen_search.html";
-
-    $(document).ready(function(){
-        $.post('/searchPage',
-        {
-            inquiry: searchFor
-        },
-        function(data){
-        });
-    });
 }
+
+var searchFor = document.getElementById("searchText").innerHTML;
+console.log("Searching for" + searchFor);
+location.href = "/../streetcloud_gen_search.html";
+
+$(document).ready(function(){
+    $.post('/foodPage',
+    {
+        inquiry: searchFor
+    },
+    function(data){
+    });
+});
+
+$(document).ready(function(){
+    $.post('/medicalPage',
+    {
+        inquiry: searchFor
+    },
+    function(data){
+    });
+});
+
+$(document).ready(function(){
+    $.post('/shelterPage',
+    {
+        inquiry: searchFor
+    },
+    function(data){
+    });
+});
+*/
