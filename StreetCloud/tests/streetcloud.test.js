4763d6ef1d0db3695eddf69a3cc28602fb553745
Alli Jacobs <jacobsa20@up.edu>
2019-11-03 15:46:51 -0800

    Test folder
    Created a test folder and streetcloud.test.js folder.

---
+++ StreetCloud/tests/streetcloud.test.js
@@ -0,0 +1,11 @@
+//Alli Jacobs & the StreetCloud Gang
+'use strict';
+
+jest.mock('../medicalPage');
+
+test( 'display medical page', () =>{
+    //goal: when medical page is clicked, the title of the page is Medical
+
+    expect()
+
+})
