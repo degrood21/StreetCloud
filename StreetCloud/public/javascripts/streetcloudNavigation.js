52ff006c48d952a24f34870b67269fc1f8a50e3f
David Campbell <campbeld21@up.edu>
2019-11-17 13:12:10 -0800

    this still doesn't work

--- StreetCloud/public/javascripts/streetcloudNavigation.js
+++ StreetCloud/public/javascripts/streetcloudNavigation.js
@@ -1,4 +1,10 @@
 
+//var distance = require('google-distance-matrix');
+//import { google } from 'google-distance-matrix';
+src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAJQvyANqrylw4vyAkEf_aScXZfusD1b7Y&libraries=geometry&language=en&callback=initMap"
+//src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"
+//src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"
+
     function calculateDistance(origin, destination) {
       var service = new google.maps.DistanceMatrixService();
       service.getDistanceMatrix(
